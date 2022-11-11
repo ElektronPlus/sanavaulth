1e972fc78ba0f04ba0d9b3cdb94ab856ecc28388
Mopsior <bartek.szg@gmail.com>
2022-11-11 17:50:55 +0100

    init

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1 className='text-green-500 text-lg'>sanavaulth</h1>
+            <p>🔥🔥</p>
+        </div>
+    )
+}
---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
+    reactStrictMode: false,
+    swcMinify: true,
+}
+
+module.exports = nextConfig
