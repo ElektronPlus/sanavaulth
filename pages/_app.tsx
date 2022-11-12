f618a8fbe384dd60f7555260e789273721b9f512
Mopsior <kontakt@mopsior.pl>
2022-11-12 22:08:24 +0100

    Update pages/_app.tsx
    Co-authored-by: Jan Szymański <hello.konhi@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,6 @@ const inter = Inter();
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <div className={`bg-zinc-900 text-white ${inter.className}`}>
-            {/* @ts-ignore */}
             <Component {...pageProps} />
         </div>
     )
