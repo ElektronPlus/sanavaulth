7ffe95e2a7edb7e1a801cde15cd51c8329f957da
Mopsior <bartek.szg@gmail.com>
2022-11-20 18:28:23 +0100

    Add apollo

---
+++ features/support-form/fragments/Message/Message.generated.tsx
@@ -0,0 +1,71 @@
+import * as Types from '../../../../src/types';
+
+import { gql } from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Message = {
+  __typename?: 'Message';
+  content?: Maybe<Scalars['String']>;
+  email?: Maybe<Scalars['String']>;
+  id: Scalars['ID'];
+  name?: Maybe<Scalars['String']>;
+  surname?: Maybe<Scalars['String']>;
+};
+
+export type MessageInput = {
+  content: Scalars['String'];
+  email: Scalars['String'];
+  name: Scalars['String'];
+  surname: Scalars['String'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createMessage?: Maybe<Message>;
+  deleteMessage?: Maybe<Scalars['String']>;
+};
+
+
+export type MutationCreateMessageArgs = {
+  captcha: Scalars['String'];
+  message: MessageInput;
+};
+
+
+export type MutationDeleteMessageArgs = {
+  id?: InputMaybe<Scalars['ID']>;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  message?: Maybe<Message>;
+  messages?: Maybe<Array<Maybe<Message>>>;
+};
+
+
+export type QueryMessageArgs = {
+  id?: InputMaybe<Scalars['ID']>;
+};
+
+export type MessageFragment = { __typename?: 'Message', content?: string | null, name?: string | null, surname?: string | null, email?: string | null };
+
+export const MessageFragmentDoc = gql`
+    fragment Message on Message {
+  content
+  name
+  surname
+  email
+}
+    `;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,18 @@ import "styles/globals.css";
 import { Inter } from "@next/font/google";
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from "next-i18next";
+import { ApolloProvider } from "@apollo/client";
+import client from "lib/apollo-client";
 
 const inter = Inter();
 
 function App({ Component, pageProps }: AppProps) {
     return (
-        <div className={`bg-zinc-900 text-white ${inter.className}`}>
-            <Component {...pageProps} />
-        </div>
+        <ApolloProvider client={client}>
+            <div className={`bg-zinc-900 text-white ${inter.className}`}>
+                <Component {...pageProps} />
+            </div>
+        </ApolloProvider>
     )
 }
 
