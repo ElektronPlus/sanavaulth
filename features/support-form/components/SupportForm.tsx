095b0d2d25cbcba930d8dd0b8657f37c573673f4
Mopsior <bartek.szg@gmail.com>
2022-11-20 18:28:23 +0100

    Add apollo

--- features/support-form/components/SupportForm.tsx
+++ features/support-form/components/SupportForm.tsx
@@ -4,33 +4,42 @@ import { Dialog } from "components/Dialog";
 import { Input } from "components/Input";
 import { TextArea } from "components/TextArea";
 import { useTranslation } from "next-i18next";
+import { useForm } from "react-hook-form";
+import { useMessageStore } from "../hooks/useMessageStore";
+import { MessageInput } from "../mutations/CreateMessage.generated";
 
 export function SupportForm() {
-    const { t } = useTranslation('common')
+    const { t } = useTranslation('common');
+    const { register, handleSubmit } = useForm<MessageInput>();
+    const { createMessage } = useMessageStore();
+
+    function onMessageCreate(data: MessageInput) {
+        createMessage(data, '10000000-aaaa-bbbb-cccc-000000000001');
+    }
 
     return (
         <Dialog title={t("supportForm.title")} description={t("supportForm.description")} button={<button className="absolute bottom-4 right-4">{t("supportForm.help")}</button>}>
-        <form className="grid gap-5">
+        <form className="grid gap-5" onSubmit={handleSubmit(onMessageCreate)}>
             <Input
             type="text"
-            name="firstName"
+            {...register('name')}
             placeholder={t("supportForm.namePlaceholder")}
             label={t("name")}
             />
             <Input
             type="text"
-            name="surName"
+            {...register('surname')}
             placeholder={t("supportForm.surnamePlaceholder")}
             label={t("surname")}
             />
             <Input
             type="email"
-            name="email"
+            {...register('email')}
             placeholder={t("supportForm.emailPlaceholder")}
             label={t("email")}
             />
             <TextArea
-            name="text"
+            {...register('content')}
             label={t("supportForm.request")}
             cols={30}
             rows={10}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import "styles/globals.css";
 import { Inter } from "@next/font/google";
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from "next-i18next";
+import { ApolloProvider } from "@apollo/client";
+import client from "lib/apollo-client";
 
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
@@ -13,14 +15,16 @@ function App({ Component, pageProps }: AppProps) {
     const [supabaseClient] = useState(() => createBrowserSupabaseClient())
     
     return (
-        <SessionContextProvider
-            supabaseClient={supabaseClient}
-            initialSession={pageProps.initialSession}
-        >
-            <div className={`bg-zinc-900 text-white ${inter.className}`}>
-                <Component {...pageProps} />
-            </div>
-        </SessionContextProvider>
+        <ApolloProvider client={client}>
+            <SessionContextProvider
+                supabaseClient={supabaseClient}
+                initialSession={pageProps.initialSession}
+            >
+                <div className={`bg-zinc-900 text-white ${inter.className}`}>
+                        <Component {...pageProps} />
+                    </div>
+            </SessionContextProvider>
+        </ApolloProvider>
     )
 }
 
