0a649dc34470535acf5bcfa900053e4274281a33
Mopsior <bartek.szg@gmail.com>
2022-11-20 20:08:54 +0100

    Set Auth to only magic link

--- features/auth/AuthForm.tsx
+++ features/auth/AuthForm.tsx
@@ -1,28 +1,27 @@
-import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
+import { Auth, ThemeSupa,  } from '@supabase/auth-ui-react'
 import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import { Database } from "lib/database.types";
 
 export function AuthForm() {
     const supabaseClient = useSupabaseClient<Database>()
     return (
-        <div>
-            <Auth 
-                supabaseClient={supabaseClient}
-                appearance={{
-                    theme: ThemeSupa,
-                    className: {
-                        container: 'w-80'
-                    },
-                    variables: {
-                        default: {
-                            colors: {
-                                inputText: 'hsl(153 60.0% 53.0%)'
-                            }
+        <Auth 
+            view='magic_link'
+            showLinks={false}
+            supabaseClient={supabaseClient}
+            appearance={{
+                theme: ThemeSupa,
+                className: {
+                    container: 'w-80'
+                },
+                variables: {
+                    default: {
+                        colors: {
+                            inputText: 'white'
                         }
                     }
-                }}
-                magicLink={true}
-            />
-        </div>
+                }
+            }}
+        />
     )
 }
