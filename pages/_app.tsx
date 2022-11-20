ed7e08dfae44c90a81739b128cd719e748a09f32
Mopsior <bartek.szg@gmail.com>
2022-11-20 17:12:11 +0100

    Add authenication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,24 @@ import { Inter } from "@next/font/google";
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from "next-i18next";
 
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from "react";
+
 const inter = Inter();
 
 function App({ Component, pageProps }: AppProps) {
+    const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+    
     return (
-        <div className={`bg-zinc-900 text-white ${inter.className}`}>
-            <Component {...pageProps} />
-        </div>
+        <SessionContextProvider
+            supabaseClient={supabaseClient}
+            initialSession={pageProps.initialSession}
+        >
+            <div className={`bg-zinc-900 text-white ${inter.className}`}>
+                <Component {...pageProps} />
+            </div>
+        </SessionContextProvider>
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,22 @@ import { SupportForm } from "../features/support-form/components/SupportForm";
 import Head from "next/head";
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
+import { Database } from "lib/database.types";
+
 export default function HomePage() {
+    const supabaseClient = useSupabaseClient<Database>()
     return (
         <>
             <Head>
                     <title>sanavaulth</title>
             </Head>
+            <Auth 
+                supabaseClient={supabaseClient}
+                appearance={{ theme: ThemeSupa }}
+                magicLink={true}
+            />
             <SupportForm />
         </>
     )
