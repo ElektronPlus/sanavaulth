87d091fbbee2794824ddea532a09c1e1cd67950f
Mopsior <bartek.szg@gmail.com>
2022-12-05 20:21:57 +0100

    Delete 💩 auth translation

--- features/auth/AuthForm.tsx
+++ features/auth/AuthForm.tsx
@@ -2,36 +2,11 @@ import { Auth, ThemeSupa,  } from '@supabase/auth-ui-react'
 import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import { Database } from "lib/database.types";
 import { useTranslation } from "next-i18next";
-import { useEffect } from 'react';
 
 export function AuthForm() {
     const { t } = useTranslation('common')
     const supabaseClient = useSupabaseClient<Database>()
 
-    useEffect(() => {
-        const targetNode = document.getElementsByClassName("MAGIC_LINK_CONTAINER")[0];
-        const config = { attributes: true, childList: true, subtree: true };
-    
-        const callback = function (mutationList: any) {
-        for (const mutation of mutationList) {
-            if (mutation.type === "childList") {
-                const message =
-                    document.getElementsByClassName("MAGIC_LINK_MESSAGE")[0];
-                if (message?.innerHTML == "unsupported_otp_type")
-                    message.innerHTML = t('loginForm.noEmail');
-                else if (message?.innerHTML == "Check your email for the magic link")
-                    message.innerHTML = t('loginForm.emailSended');
-                else if (message?.innerHTML == "For security purposes, you can only request this once every 60 seconds")
-                    message.innerHTML = t('loginForm.cooldown');
-                else if (message?.innerHTML == "Signups not allowed for this instance")
-                    message.innerHTML = t('loginForm.noSignup');
-            }
-        }
-        };
-    
-        const observer = new MutationObserver(callback);
-        observer.observe(targetNode, config);
-    });
     return (
         <div className='w-96 px-3 border-solid border-[#3fcf8e] border-2 rounded-lg drop-shadow-lg'>
             <h1 className='text-center mt-4 text-xl'>{t('loginForm.title')}</h1>
